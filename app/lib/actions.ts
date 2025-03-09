c638a4b33c2d4fba0fcd77b2fb619c9f97a8f417
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-03-09 15:51:34 +0900

    feat: create invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,11 @@
 "use server";
 
 import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
 	id: z.string(),
@@ -20,4 +25,12 @@ export async function createInvoice(formData: FormData) {
 	});
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
+
+	await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+	revalidatePath("/dashboard/invoices");
+	redirect("/dashboard/invoices");
 }
